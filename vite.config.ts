764da6782d869a209e571d64dc9e4d8797f7bee1
m1n9o <gxmk9999@gmail.com>
2026-09-11 11:59:23 +0800

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/quad-daily/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
   },
-})
+}))
